68216cfec2806e425b6fb0e5444daba5641930b2
pierresouza <pierre.s3@hotmail.com>
2026-03-22 15:28:05 -0300

    feat: configure Next.js output and image optimization settings

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export", // Adicione esta linha
+  // Se você usa imagens do Next.js (<Image />),
+  // pode precisar desativar a otimização nativa, pois ela requer um servidor Node.js:
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
